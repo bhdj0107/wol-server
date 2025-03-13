4c2517cf37fc2a320a73bc938feafd5c410a1b6a
bhdj0107 <bhdj0107@gmail.com>
2025-03-13 00:55:45 +0000

    Optimize / Bug fix

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.12.9
 
 WORKDIR /app
+RUN pip install flask_restx
+
 COPY main.py /app/
 COPY wol.py /app/
-
-RUN pip install flask_restx
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from flask_restx import Api, Resource, Namespace
 
 app = Flask(__name__)
 api = Api(app)
-
-def environSetter(key, default):
-    try: return os.environ[key]
-    else: return default
     
 # Get Environment
 MAC_ADDR = os.environ['MAC_ADDR']
